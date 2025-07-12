d02502013b43cfc3862c5505bae49fb27560c79c
amrutheshc <amrutheshamru@gmail.com>
2025-07-12 17:02:25 +0530

    checkout sec

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
