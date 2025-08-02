fe728b46efc7e8fc84260d8eb2da90a99875dcee
amrutheshc <amrutheshamru@gmail.com>
2025-08-03 00:54:01 +0530

    sliders

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,192 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,SlickCarouselModule,CommonModule,CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  testimonials = [
+    {
+      rating: 5,
+      quote: 'Partnering with Mazra Care has been a turning point in our sustainability journey...',
+      name: 'Mike Hardson',
+      designation: 'Customer',
+      image: '../assets/images/team/person.jpeg'
+    },
+    {
+      rating: 5,
+      quote: 'Mazra Care embodies the future of food security in the UAE...',
+      name: 'Joseph Kennedy',
+      designation: 'Founder',
+      image: '../assets/images/team/person.jpeg'
+    },
+    {
+      rating: 5,
+      quote: 'Thanks to Mazra Care’s adaptable systems, we now grow fresh vegetables...',
+      name: 'Mike Hardson',
+      designation: 'Customer',
+      image: '../assets/images/team/person.jpeg'
+    },
+    {
+      rating: 5,
+      quote: 'As a mother, I was worried about the safety of store-bought vegetables...',
+      name: 'Joseph Kennedy',
+      designation: 'Founder',
+      image: '../assets/images/team/person.jpeg'
+    },
+    {
+      rating: 5,
+      quote: 'We rely on Mazra Care’s farms for ultra-fresh, pesticide-free greens...',
+      name: 'Mike Hardson',
+      designation: 'Customer',
+      image: '../assets/images/team/person.jpeg'
+    },
+    {
+      rating: 5,
+      quote: 'What attracted me to invest in Mazra Care was their scalable, impact-driven model...',
+      name: 'Mark Smith',
+      designation: 'Manager',
+      image: '../assets/images/team/person.jpeg'
+    }
+  ];
+
+
+  mainSliderConfig = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.testimonials-two__carousel-thumb',
+  autoplay: true,
+  infinite: true,
+  dots: false,
+  arrows: false,
+  autoplaySpeed: 5000,
+  centerPadding: 0
+};
+
+thumbSliderConfig = {
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: '.testimonials-two__carousel',
+  centerMode: true,
+  focusOnSelect: true,
+  autoplay: true,
+  infinite: true,
+  dots: false,
+  arrows: false,
+  centerPadding: 0,
+  responsive: [
+    {
+      breakpoint: 501,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
+
+ projects = [
+    {
+      title: 'Residential',
+      link: '/residential',
+      image: '../assets/images/pintrest/Image_2.png.jpg'
+    },
+    {
+      title: 'Commercial',
+      link: '/commercial',
+      image: '../assets/images/pintrest/agri2.jpg'
+    },
+    {
+      title: 'Industrial',
+      link: '/industrial',
+      image: '../assets/images/pintrest/indr4.jpg'
+    },
+    {
+      title: 'Institutional',
+      link: '/institutional',
+      image: '../assets/images/pintrest/school2.jpg'
+    }
+  ];
+
+  customOptions = {
+    loop: false,
+    margin: 30,
+    smartSpeed: 700,
+    nav: false,
+    dots: true,
+    autoplay: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1024: {
+        items: 3
+      },
+      1400: {
+        items: 4
+      }
+    }
+  };
+
+
+
+
+
+  blogPosts = [
+    {
+      title: 'Agriculture is Important same as Education',
+      image: '../assets/images/blog/blog-1-1.jpg',
+      admin: 'Sam',
+      comments: 5,
+      description: `Let kids get their hands dirty while learning about sustainable farming and healthy eating. It's all
+        about inspiring curiosity, teaching responsibility.`,
+      srText: 'Cultivating a Greener Future with Mazra Care’s Smart Farming Solutions'
+    },
+    {
+      title: "Why Mazra Care Rock's for students",
+      image: '../assets/images/blog/blog-1-2.jpg',
+      admin: 'Alex',
+      comments: 9,
+      description: `Connecting with the environment in a fun & interactive way. Hands-on lessons in sustainable farming.
+        Learning about food and where it comes from.`,
+      srText: 'Empowering Communities Through Agriculture and Wellness'
+    },
+    {
+      title: 'Boost your Farm with Quality Products',
+      image: '../assets/images/blog/blog-1-3.jpg',
+      admin: 'Sam',
+      comments: 5,
+      description: `Learn proven strategies to increase your revenue. Mazra Care offers pure, organic
+        alternatives for skincare, nutrition, and personal care—all made sustainably.`,
+      srText: 'Pure, Ethical, and Powerful: The Mazra Care Wellness Revolution'
+    }
+  ];
+
+  blogCarouselOptions = {
+    loop: false,
+    margin: 30,
+    smartSpeed: 700,
+    nav: false,
+    dots: false,
+    autoplay: false,
+    responsive: {
+      0: { items: 1 },
+      768: { items: 2 },
+      992: { items: 3 }
+    }
+  };
+
+
+  
 }
--- src/assets/js/alefox.js
+++ src/assets/js/alefox.js
@@ -38,6 +38,7 @@
 //   }
 
 //   // Popular Causes Progress Bar
+  
 //   if ($(".count-bar").length) {
 //     $(".count-bar").appear(
 //       function () {
@@ -129,43 +130,44 @@
 //   }
 
 //   // custom coursor
-//   if ($(".custom-cursor").length) {
-//     var cursor = document.querySelector(".custom-cursor__cursor");
-//     var cursorinner = document.querySelector(".custom-cursor__cursor-two");
-//     var a = document.querySelectorAll("a");
 
-//     document.addEventListener("mousemove", function (e) {
-//       var x = e.clientX;
-//       var y = e.clientY;
-//       cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
-//     });
+//   // if ($(".custom-cursor").length) {
+//   //   var cursor = document.querySelector(".custom-cursor__cursor");
+//   //   var cursorinner = document.querySelector(".custom-cursor__cursor-two");
+//   //   var a = document.querySelectorAll("a");
 
-//     document.addEventListener("mousemove", function (e) {
-//       var x = e.clientX;
-//       var y = e.clientY;
-//       cursorinner.style.left = x + "px";
-//       cursorinner.style.top = y + "px";
-//     });
+//   //   document.addEventListener("mousemove", function (e) {
+//   //     var x = e.clientX;
+//   //     var y = e.clientY;
+//   //     cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+//   //   });
 
-//     document.addEventListener("mousedown", function () {
-//       cursor.classList.add("click");
-//       cursorinner.classList.add("custom-cursor__innerhover");
-//     });
+//   //   document.addEventListener("mousemove", function (e) {
+//   //     var x = e.clientX;
+//   //     var y = e.clientY;
+//   //     cursorinner.style.left = x + "px";
+//   //     cursorinner.style.top = y + "px";
+//   //   });
 
-//     document.addEventListener("mouseup", function () {
-//       cursor.classList.remove("click");
-//       cursorinner.classList.remove("custom-cursor__innerhover");
-//     });
+//   //   document.addEventListener("mousedown", function () {
+//   //     cursor.classList.add("click");
+//   //     cursorinner.classList.add("custom-cursor__innerhover");
+//   //   });
 
-//     a.forEach((item) => {
-//       item.addEventListener("mouseover", () => {
-//         cursor.classList.add("custom-cursor__hover");
-//       });
-//       item.addEventListener("mouseleave", () => {
-//         cursor.classList.remove("custom-cursor__hover");
-//       });
-//     });
-//   }
+//   //   document.addEventListener("mouseup", function () {
+//   //     cursor.classList.remove("click");
+//   //     cursorinner.classList.remove("custom-cursor__innerhover");
+//   //   });
+
+//   //   a.forEach((item) => {
+//   //     item.addEventListener("mouseover", () => {
+//   //       cursor.classList.add("custom-cursor__hover");
+//   //     });
+//   //     item.addEventListener("mouseleave", () => {
+//   //       cursor.classList.remove("custom-cursor__hover");
+//   //     });
+//   //   });
+//   // }
 
 //   if ($(".contact-form-validated").length) {
 //     $(".contact-form-validated").validate({
@@ -849,7 +851,7 @@
 
 //     var scrollToTopBtn = ".scroll-to-top";
 //     if (scrollToTopBtn.length) {
-//       if ($(window).scrollTop() > 500) {
+//     if ($(window).scrollTop() > 500) {
 //         $(scrollToTopBtn).addClass("show");
 //       } else {
 //         $(scrollToTopBtn).removeClass("show");
@@ -877,6 +879,7 @@
 //   }
 
 //   /*------ Count Down -----*/
+
 //   if ($(".countdown-one__list").length) {
 //     let mainDate = $(".countdown-one__list").data("deadline-date");
 //     let yearsCondition =
