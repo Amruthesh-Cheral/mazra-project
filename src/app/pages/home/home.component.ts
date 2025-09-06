967f8fce453c4a60295f8caddaa238e25af1c681
amrutheshc <amrutheshamru@gmail.com>
2025-09-06 14:54:33 +0530

    changes video

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,6 +21,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   @ViewChild('blogCarousel') blogCarousel!: ElementRef;
 
 
+  
   projectSlides = [
     {
       title: 'Residential',
